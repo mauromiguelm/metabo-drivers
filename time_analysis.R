6450f985ffcd7f68a6b6d7eee45b9f5ac2cb5951
masierom <masiero@imsb.biol.ethz.ch>
2020-01-03 18:26:21 +0100

    fixed the parsing of excel time sheet refering to sam[pling and scanniung of 384 plates

--- time_analysis.R
+++ time_analysis.R
@@ -92,7 +92,7 @@ time_data[,"time_treatment384_p2_end"] <- tmp
 #organizing 96 TREATMENT time
 
 time_data[,"time_treatment_96p1_end"] <- unlist(lapply(raw_data$`time_treatment_96_p1_end***`, convert_time))
-time_data[,"time_treatment_96p1_end"] <- chron(times = time_data[,"time_treatment96_p1_end"]) 
+time_data[,"time_treatment_96p1_end"] <- chron(times = time_data[,"time_treatment_96p1_end"]) 
 
 tmp <- paste(time_data[,"treatment_day"], time_data[,"time_treatment_96p1_end"])
 
@@ -103,7 +103,7 @@ tmp <- as.POSIXct(tmp)
 time_data[,"time_treatment_96p1_end"] <- tmp 
 
 time_data[,"time_treatment_96p2_end"] <- unlist(lapply(raw_data$`time_treatment_96_p2_end***`, convert_time))
-time_data[,"time_treatment_96p2_end"] <- chron(times = time_data[,"time_treatment96_p2_end"]) 
+time_data[,"time_treatment_96p2_end"] <- chron(times = time_data[,"time_treatment_96p2_end"]) 
 
 tmp <- paste(time_data[,"treatment_day"], time_data[,"time_treatment_96p2_end"])
 
@@ -113,7 +113,7 @@ tmp <- as.POSIXct(tmp)
 
 time_data[,"time_treatment_96p2_end"] <- tmp 
 
-#organizing SAMPLING TIME time
+#organizing SAMPLING TIME 96wp 
 
 time_data[,"p1_sampling_start"] <- unlist(lapply(raw_data$`p1_sampling start`, convert_time))
 time_data[,"p1_sampling_start"] <- chron(times = time_data[,"p1_sampling_start"]) 
@@ -160,3 +160,176 @@ tmp <- as.POSIXct(tmp)
 
 time_data[,"p2_sampling_end"] <- tmp
 
+# creating list vector with approximate sampling time
+
+# here the order is q1, q2, q3 and q4 for p1 and p2, respectivelly.
+
+
+
+lapply(rownames(time_data), function(row){
+  
+  #row <- 8
+  
+  data <- time_data[row,]
+  
+  if(any(is.na(c(data[["p1_sampling_end"]], data[["p1_sampling_start"]])))){
+    
+    return(NA)
+    
+    }else{
+      
+      sampling_length <- difftime(data[["p1_sampling_end"]], data[["p1_sampling_start"]], units = "secs")
+      
+      sampling_length <- seq(0, as.numeric(sampling_length), length.out = 4)
+      
+      plate_sampling_start <- lapply(sampling_length, function(time)
+      {data["p1_sampling_start"] + time})
+      
+      plate_sampling_start <- do.call(rbind, plate_sampling_start)[,1]
+      
+      return(plate_sampling_start)
+
+      
+    }
+    
+    
+  }) -> time_sampling_96p1
+
+lapply(as.numeric(rownames(time_data)), function(row){
+  
+  #row <- 4
+
+  data <- time_data[row,]
+  
+  time_sampling <- time_sampling_96p1[[row]]
+  
+  if(any(is.na(c(data[["time_treatment384_p1_end"]], time_sampling)))){
+    
+    return(NA)
+    
+  }else{
+  
+  time_sampling <- time_sampling_96p1[[row]]
+  
+  treatment_time <- difftime(time_sampling, data$time_treatment_96p1_end, units = "h")
+  
+  return(treatment_time)
+  
+  }
+  
+}) -> time_diff_96
+
+time_diff_96 <- data.frame(sampling_p1= unlist(time_diff_96))
+
+## plate 2
+
+lapply(rownames(time_data), function(row){
+  
+  #row <- 8
+  
+  data <- time_data[row,]
+  
+  if(any(is.na(c(data[["p2_sampling_end"]], data[["p2_sampling_start"]])))){
+    
+    return(NA)
+    
+  }else{
+    
+    sampling_length <- difftime(data[["p2_sampling_end"]], data[["p2_sampling_start"]], units = "secs")
+    
+    sampling_length <- seq(0, as.numeric(sampling_length), length.out = 4)
+    
+    plate_sampling_start <- lapply(sampling_length, function(time)
+    {data["p2_sampling_start"] + time})
+    
+    plate_sampling_start <- do.call(rbind, plate_sampling_start)[,1]
+    
+    return(plate_sampling_start)
+    
+  }
+  
+  
+}) -> time_sampling_96p2
+
+unlist(lapply(as.numeric(rownames(time_data)), function(row){
+  
+  #row <- 4
+  
+  data <- time_data[row,]
+  
+  time_sampling <- time_sampling_96p2[[row]]
+  
+  if(any(is.na(c(data[["time_treatment384_p2_end"]], time_sampling)))){
+    
+    return(NA)
+    
+  }else{
+    
+    time_sampling <- time_sampling_96p2[[row]]
+    
+    treatment_time <-difftime(time_sampling, data$time_treatment_96p2_end, units = "h")
+    
+    
+    return(treatment_time)
+    
+  }
+  
+})) -> time_diff_96$sampling_p2
+
+# plotting times 384 incubation times ----------------------------------------------------------
+
+plot = F # plot = T
+
+if(plot == T){
+  
+  require(ggplot2)
+  require(ggExtra)
+  
+  diff_times_384 <- data.frame(incubation_time_384p1 = as.numeric(time_data$time_treatment384_p1_end - time_data$time_seed_96_finish))
+  diff_times_384$incubation_time_384p2 <- as.numeric(time_data$time_treatment384_p2_end - time_data$time_seed_96_finish)
+  
+  (p <- ggplot(diff_times_384, aes(x=incubation_time_384p1, y=incubation_time_384p2)) +
+      geom_point() +
+      theme(legend.position="none")+
+      scale_x_continuous(breaks = seq(floor(min(diff_times_384, na.rm = T)), ceiling(max(diff_times_384, na.rm = T)),2))+
+      scale_y_continuous(breaks = seq(floor(min(diff_times_384, na.rm = T)), ceiling(max(diff_times_384, na.rm = T)),2))+
+      theme_bw())
+  
+  # Set relative size of marginal plots (main plot 10x bigger than marginals)
+  (p <- ggMarginal(p, type="histogram", size=2))
+  
+  ggsave(filename = "paperReady_384_incubation_times.png", plot = p, device = "png",
+         path = "\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\figures")
+  
+}
+
+
+# plotting times 96 treatment times --------------------------------------
+
+if(plot == T){
+  
+  require(ggplot2)
+  require(ggExtra)
+  
+  (p <- ggplot(time_diff_96, aes(x= sampling_p1, y=sampling_p2)) +
+      geom_point() +
+      theme(legend.position="none")+
+      scale_x_continuous(breaks = seq(floor(min(diff_times_384, na.rm = T)), ceiling(max(diff_times_384, na.rm = T)),2))+
+      scale_y_continuous(breaks = seq(floor(min(diff_times_384, na.rm = T)), ceiling(max(diff_times_384, na.rm = T)),2))+
+      theme_bw())
+  
+  # Set relative size of marginal plots (main plot 10x bigger than marginals)
+  (p <- ggMarginal(p, type="histogram", size=2))
+  
+  ggsave(filename = "paperReady_384_incubation_times.png", plot = p, device = "png",
+         path = "\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\figures")
+  
+}
+
+# organizing data for output ----------------------------------
+
+time_vectors <- list(plate384 = time_data,
+                     samp_p1_96 = time_sampling_96p1,
+                     samp_p2_96 = time_sampling_96p2)
+
+rm(list = ls()[ls() != "time_vectors"])
