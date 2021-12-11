d223470600a4ad832fed774004441681dd01db16
mauromiguelm <masiero@imsb.biol.ethz.ch>
2021-12-11 21:55:55 +0100

    tasks for metabolomics analysis added

---
+++ analysis_metabolomics.R
@@ -0,0 +1,8 @@
+
+# Correlation of FCS for all data and for every drug. See if we have any R/S grouping.. get insights on this
+# Compare metabolomics results for R/Sgroups, and see which one is better
+# check which metabolites are driving the correlation, hopefully positive controls
+# Correlate FC with GR50 and GR24
+# For metabolic drugs: check if direct substrate/products involved in MoA are relating to any drug metric 
+# Multiomics: Link protein/gene/mRNA levels to metabolomics/drug sensitivity
+# Multiomics: distance_to_target analysis
