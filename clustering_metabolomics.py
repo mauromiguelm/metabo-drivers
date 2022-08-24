f4a71f9c904c2f76f93bf9994b72a33f7095da49
mauromiguelm <masiero@imsb.biol.ethz.ch>
2022-08-24 10:52:47 +0200

    added option for clustering without drug effect penalization, improved plotting

--- clustering_metabolomics.py
+++ clustering_metabolomics.py
@@ -1,11 +1,12 @@
-import os, umap, hdbscan, hyperopt, pickle
+import os, umap, hdbscan, hyperopt, pickle, h5py, re
 import numpy as np
+import pandas
 import pandas as pd
 import seaborn as sns
 import scipy.stats as stats
 from itertools import compress
-
 import matplotlib.pyplot as plt
+import umap.plot
 
 def import_and_transform_data(file):
     data = pd.read_csv(file,index_col=0)
@@ -39,7 +40,7 @@ def score_clusters(clusters,prob_threshold = 0.05):
     return(label_count, cost)
 
 
-def objective(params,metadata, data):
+def objective(params,metadata, data, sensitivity_penalty=True):
     """
     Objective function for hyperopt to minimize, which incorporates constraints
     on the number of clusters we want to identify
@@ -54,32 +55,39 @@ def objective(params,metadata, data):
 
     label_count, cost = score_clusters(clusters = clusters, prob_threshold=params['prob_threshold'])
 
-    # penalty on the cost function if absense of concentration effect
-    samples = [value[1].dropna() for value in metadata.groupby(clusters.labels_)['percent_change_GR']]
+    if(sensitivity_penalty== True):
 
-    samples = [x  for x in samples if len(x) > 0]
+        # penalty on the cost function if absense of concentration effect
+        samples = [value[1].dropna() for value in metadata.groupby(clusters.labels_)['percent_change_GR']]
 
-    if len(samples)>1:
-        f_val, p_val = stats.f_oneway(*samples)
+        samples = [x  for x in samples if len(x) > 0]
+
+        if len(samples)>1:
+            f_val, p_val = stats.f_oneway(*samples)
+        else:
+            p_val=1
+
+        if p_val>0.05:
+            penalty = params['penalty']
+        else:
+            penalty = 0
+
+        loss = cost + penalty
+
+        return({'loss': loss, 'label_count': label_count, 'status':  hyperopt.STATUS_OK})
     else:
-        p_val=1
+        loss = cost
 
-    if p_val>0.05:
-        penalty = params['penalty']
-    else:
-        penalty = 0
+        return ({'loss': loss, 'label_count': label_count, 'status': hyperopt.STATUS_OK})
 
-    loss = cost + penalty
 
-    return({'loss': loss, 'label_count': label_count, 'status':  hyperopt.STATUS_OK})
-
-def bayesian_search(data, space, n_evals):
+def bayesian_search(metadata, data, space, n_evals, sensitivity_penalty):
     """
         Perform bayseian search on hyperopt hyperparameter space to minimize objective function
     """
 
     trials = hyperopt.Trials()
-    fmin_objective = hyperopt.partial(objective, data=data, metadata = metadata)
+    fmin_objective = hyperopt.partial(objective, data=data, metadata = metadata, sensitivity_penalty=sensitivity_penalty)
     best = hyperopt.fmin(fmin_objective,
                 space=space,
                 algo= hyperopt.tpe.suggest,
@@ -153,10 +161,110 @@ def main():
     pass
 
 if __name__ = "__main__":
-    path_fig = "C:\\Users\\mauro\\Documents\\phd_results"
-    path_data = "C:\\Users\\mauro\\Documents\\phd_results"
 
-    files = os.listdir(path_data+"\\log2fc_full")
+    path_fig = "\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\figures_mean"
+    path_metab = "\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\metabolomicsData_processed"
+    os.chdir(path_metab)
+    raw_data = h5py.File('metabolomics_raw.h5','r+')
+    normalized_data = h5py.File('MM4_Mean mean_norm_DATA.h5', 'r+')
+
+    # clustering of raw metabolomics data
+
+    metadata = pd.DataFrame(raw_data['samples/name'])
+
+    metadata = [str(x).split("_") for x in metadata.iloc[:,0]]
+
+    metadata = pd.DataFrame(metadata)
+
+    metadata = metadata.fillna("Solvent")
+
+    metadata["ctrls"] = ["poolCtrl" if x == 'poolCtrl' else "cells" for x in metadata.iloc[:, 6]]
+
+    data = pd.DataFrame(raw_data.get('data'))
+
+    hspace = {'n_neighbors': hyperopt.hp.choice('n_neighbors', range(2, 500)),
+              'n_components': hyperopt.hp.choice('n_components', range(2, 3)),
+              'min_cluster_size': hyperopt.hp.choice('min_cluster_size', range(10, 1000)),
+              "prob_threshold": 0.1,
+              'penalty': 0.15,
+              'n_evals': 100,
+              'random_state': 13,
+              'sensitivity_penalty' : False
+    }
+
+
+
+    a, b = bayesian_search(metadata= metadata, data=data, space=hspace, n_evals=hspace['n_evals'],sensitivity_penalty=hspace['sensitivity_penalty'])
+
+
+
+    #a = {'min_cluster_size': 164, 'n_components': 2, 'n_evals': 100, 'n_neighbors': 87, 'penalty': 0.15, 'prob_threshold': 0.1, 'random_state': 13, 'sensitivity_penalty': False}
+
+    os.chdir(path_fig + "\\clustering_figures")
+
+    from matplotlib.axes._axes import _log as matplotlib_axes_logger
+
+    matplotlib_axes_logger.setLevel('ERROR')
+
+    mapper = umap.UMAP(n_neighbors=a['n_neighbors'],
+                                         n_components=a['n_components']).fit(data.values)
+    umap.plot.points(mapper, labels=metadata.iloc[:,5],show_legend=False)
+    plt.savefig('raw_cells.png')
+
+    umap.plot.points(mapper, labels=metadata['ctrls'],show_legend=False)
+    plt.savefig('raw_ctrls.png')
+
+    #cluster norm metabolomics  data
+
+    metadata = pd.DataFrame(normalized_data['samples/name'])
+
+    metadata = [str(x).split("_") for x in metadata.iloc[:,0]]
+
+    metadata = pd.DataFrame(metadata)
+
+    metadata = metadata.fillna("Solvent")
+
+    metadata["ctrls"] = ["poolCtrl" if x == 'poolCtrl' else "cells" for x in metadata.iloc[:, 6]]
+
+    data = pd.DataFrame(normalized_data.get('data'))
+
+    hspace = {'n_neighbors': hyperopt.hp.choice('n_neighbors', range(2, 500)),
+              'n_components': hyperopt.hp.choice('n_components', range(2, 3)),
+              'min_cluster_size': hyperopt.hp.choice('min_cluster_size', range(10, 1000)),
+              "prob_threshold": 0.1,
+              'penalty': 0.15,
+              'n_evals': 100,
+              'random_state': 13,
+              'sensitivity_penalty' : False
+    }
+
+    a, b = bayesian_search(metadata= metadata, data=data, space=hspace, n_evals=hspace['n_evals'],sensitivity_penalty=hspace['sensitivity_penalty'])
+
+    #{'min_cluster_size': 138, 'n_components': 2, 'n_evals': 100, 'n_neighbors': 38, 'penalty': 0.15,
+    # 'prob_threshold': 0.1, 'random_state': 13, 'sensitivity_penalty': False}
+
+    os.chdir(path_fig + "\\clustering_figures")
+
+    from matplotlib.axes._axes import _log as matplotlib_axes_logger
+
+    matplotlib_axes_logger.setLevel('ERROR')
+
+    mapper = umap.UMAP(n_neighbors=a['n_neighbors'],
+                                         n_components=a['n_components']).fit(data.values)
+    umap.plot.points(mapper, labels=metadata.iloc[:,5], show_legend=False)
+    plt.savefig('norm_cells.png')
+
+    umap.plot.points(mapper, labels=metadata['ctrls'],show_legend=False)
+    plt.savefig('norm_ctrls.png')
+
+
+
+    #cluster log2fc
+
+    path_fig = "\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\figures_mean"
+    path_data = "\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\clean_data_mean"
+
+    files = os.listdir(path_data+"\\log2fc")
     files = list(compress(files, ["metadata" in x for x in files]))
 
     #drug-by-drug clustering
@@ -260,14 +368,16 @@ if __name__ = "__main__":
 
     # full data clustering
 
-    hspace = {'n_neighbors': hyperopt.hp.choice('n_neighbors', range(2, 40)),
-              'n_components': hyperopt.hp.choice('n_components', range(2, 20)),
+    hspace = {'n_neighbors': hyperopt.hp.choice('n_neighbors', range(2, 500)),
+              'n_components': hyperopt.hp.choice('n_components', range(2, 3)),
               'min_cluster_size': hyperopt.hp.choice('min_cluster_size', range(10, 1000)),
               "prob_threshold": 0.1,
               'penalty': 0.15,
               'n_evals': 100,
-              'random_state': 13
+              'random_state': 13,
+              'sensitivity_penalty': True
               }
+
 
     drugs = set([x.split("_")[1] for x in files])
 
@@ -282,13 +392,29 @@ if __name__ = "__main__":
         """
         print(drug)
         # drug = 'Methotrexate'
-        data.append(import_and_transform_data(path_data + "\\log2fc_full" + "\\data_" + drug + '_log2fc.csv'))
-        metadata.append(import_and_transform_data(path_data + "\\log2fc_full" + "\\metadata_" + drug + '_log2fc.csv'))
+        data.append(import_and_transform_data(path_data + "\\log2fc" + "\\data_" + drug + '_log2fc.csv'))
+        metadata.append(import_and_transform_data(path_data + "\\log2fc" + "\\metadata_" + drug + '_log2fc.csv'))
 
-        data = pd.concat(data)
-        metadata = pd.concat(metadata)
+    data = pd.concat(data)
+    metadata = pd.concat(metadata)
 
-        a, b = bayesian_search(data, space=hspace, n_evals=hspace['n_evals'])
+
+    a, b = bayesian_search(data=data,metadata = metadata, space=hspace, n_evals=hspace['n_evals'],sensitivity_penalty=hspace['sensitivity_penalty'])
+
+    #{'min_cluster_size': 54, 'n_components': 2, 'n_evals': 100, 'n_neighbors': 297, 'penalty': 0.15,
+    # 'prob_threshold': 0.1, 'random_state': 13, 'sensitivity_penalty': True}
+
+    os.chdir(path_fig+'\\clustering_figures')
+
+    mapper = umap.UMAP(n_neighbors=a['n_neighbors'],
+                       n_components=a['n_components']).fit(data.values)
+
+    umap.plot.points(mapper, labels=metadata['cell_line'], show_legend=False)
+    plt.savefig('fc_cells.png')
+
+
+    umap.plot.points(mapper, labels=metadata['drug'], show_legend=False)
+    plt.savefig('log2fc_drug.png')
 
     os.chdir(path_data)
 
