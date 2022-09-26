d62bec643f6788d5719542983ae71d1d28461198
Mauro Miguel Masiero <mauromiguelm@hotmail.com>
2022-09-26 17:17:01 +0200

    add parameter file for computing sensitivity metrics

---
+++ parameters/compute_sensitivity_metrics.R
@@ -0,0 +1,3 @@
+time_treatment <- 0 #when was the drug added
+cutoff_GR_max_growth_effect <- 50 #pairs of drug & conc with abs change < this number will be excluded
+cutoff_GR_conc_min_CV <- 10 #pairs of drug & conc with CV < this number will be excluded
